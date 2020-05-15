3d1e43c94b775557a5563ad7c2826e3366c98f44
Dieinimy Maganha <dieinimy@gmail.com>
2020-05-15 09:06:56 -0300

    Para ler um determinado arquivo

---
+++ read_page_specific.py
@@ -0,0 +1,21 @@
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+import io
+import glob
+from datetime import datetime
+
+fp = open("C:\\repositorio\\teste_pdf\\pdf_split\\fatura_71722865_detalhada_346.pdf", 'rb')
+rsrcmgr = PDFResourceManager()
+retstr = io.StringIO()
+codec = 'utf-8'
+laparams = LAParams()
+device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+pages = PDFPage.get_pages(fp)
+for page in pages:
+    interpreter.process_page(page)
+    data =  retstr.getvalue()
+    print(data)
